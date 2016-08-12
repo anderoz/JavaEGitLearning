8156c77ba6b979834501505609aeef60347030bc
Adam <Adam@SSATLAS>
2016-08-11 23:53:23 -0500

    Adjusted whitespace/formatting. No logical changes.

--- EGitLearning/src/Car.java
+++ EGitLearning/src/Car.java
@@ -1,37 +1,34 @@
 
 public class Car {
-	
+
 	// Class variables
 	String carName;
-	
+
 	// Class constructors
-	public Car()
-	{
+	public Car() {
 		carName = "unknown";
 	}
-	
+
 	public Car(String carName) {
 		this.carName = carName;
 	}
-	
+
 	// Class methods
 	public void accelerate() {
 		System.out.println("Vroom!");
 	}
-	
-	
-	
+
 	@Override
 	public String toString() {
 		return "Car [carName=" + carName + "]";
 	}
 
 	public static void main(String[] args) {
-		
+
 		Car car1 = new Car("winner");
-		
+
 		System.out.println(car1);
-		
+
 		car1.accelerate();
 	}
 
